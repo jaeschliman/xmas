7a0784ea72db29efa13cae06f6c0e75c7e0948ce
Jason Aeschliman <j.aeschliman@gmail.com>
2018-01-11 08:06:02 -0800

    add windowed drawing for tmx renderer

--- tmx-renderer.lisp
+++ tmx-renderer.lisp
@@ -14,19 +14,34 @@
              #:tmx-renderer-from-file
              #:tmx-renderer-tile-properties
              #:tmx-renderer-tile-at-point
-             #:draw-tmx-renderer))
+             #:draw-tmx-renderer
+             #:draw-tmx-renderer-windowed))
 (in-package xmas.tmx-renderer)
 
-(defun draw-tmx-layer (at-x at-y tile-width tile-height layer frames)
-  (let* ((cols (xmas.tmx-reader:layer-width layer))
-         (rows (xmas.tmx-reader:layer-height layer))
-         (start-x (+ (- at-x (/ (* tile-width cols) 2.0)) (/ tile-width 2.0)))
-         (start-y (- (+ at-y (/ (* tile-height rows) 2.0)) (/ tile-width 2.0))))
+(defun draw-tmx-layer (at-x at-y tile-width tile-height layer frames
+                       &key
+                         (start-x 0) (end-x (xmas.tmx-reader:layer-width  layer))
+                         (start-y 0) (end-y (xmas.tmx-reader:layer-height layer)))
+  (maxf start-x 0)
+  (maxf start-y 0)
+  (minf end-x (xmas.tmx-reader:layer-width  layer))
+  (minf end-y (xmas.tmx-reader:layer-height layer))
+  (let* ((layer-width  (xmas.tmx-reader:layer-width  layer))
+         (layer-height (xmas.tmx-reader:layer-height layer))
+         (cols (- end-x start-x))
+         (rows (- end-y start-y))
+         (origin-x (+ (- at-x (/ (* tile-width layer-width) 2.0))
+                      (/ tile-width 2.0)))
+         (origin-y (- (+ at-y (/ (* tile-height layer-height) 2.0))
+                      (/ tile-width 2.0))))
     (dotimes (row rows)
       (dotimes (col cols)
-        (when-let (frame (aref frames (xmas.tmx-reader:layer-gid-at layer col row)))
-          (let ((x (+ start-x (* col tile-width)))
-                (y (- start-y (* row tile-height))))
+        (when-let (frame (aref frames (xmas.tmx-reader:layer-gid-at
+                                       layer
+                                       (+ start-x col)
+                                       (+ start-y row))))
+          (let ((x (+ origin-x (* (+ start-x col) tile-width)))
+                (y (- origin-y (* (+ start-y row) tile-height))))
             (xmas.draw:draw-texture-frame frame x y t)))))))
 
 (defun make-tileset-texture-frames (tileset)
@@ -98,3 +113,16 @@
                   (tmx-renderer-tile-height renderer)
                   (tmx-renderer-layer renderer)
                   (tmx-renderer-frames renderer)))
+
+(defun draw-tmx-renderer-windowed (x y renderer x1 y1 x2 y2)
+  (let* ((h (tmx-renderer-height renderer))
+         (tw (tmx-renderer-tile-width renderer))
+         (th (tmx-renderer-tile-height renderer))
+         (height-in-tiles (floor h th))
+         (tx1 (floor x1 tw))
+         (ty1 (- height-in-tiles (floor y1 th) 0))
+         (tx2 (floor x2 tw))
+         (ty2 (- height-in-tiles (floor y2 th) 0)))
+    (draw-tmx-layer x y tw th
+                    (tmx-renderer-layer renderer) (tmx-renderer-frames renderer)
+                    :start-x tx1 :end-x tx2 :start-y ty2 :end-y ty1)))
