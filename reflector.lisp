97cb8bf0335ff9455ce1c0385dcd7ab4c91f187d
Jason Aeschliman <j.aeschliman@gmail.com>
2018-01-26 15:03:33 -0800

    reflector: clean up and make prettier

--- reflector.lisp
+++ reflector.lisp
@@ -9,32 +9,36 @@
   ((reflection-count :initarg :reflection-count :initform 4)))
 
 (defmethod visit ((self reflector-node))
-  (when (not (visible self))
-    (return-from visit))
-  (xmas.render-buffer::push-matrix)
-  (let ((ax (anchor-x self))
-        (ay (anchor-y self)))
-    (assert (zerop ax))
-    (assert (zerop ay))
-    (xmas.render-buffer::translate-scale-rotate
-     (x self) (y self)
-     (if (flip-x self) (* -1.0 (scale-x self)) (scale-x self))
-     (if (flip-y self) (* -1.0 (scale-y self)) (scale-y self))
-     (rotation self)))
-  (when (children self)
-    (let* ((count (slot-value self 'reflection-count ))
-           (rot (/ 360.0 count)))
-      (loop repeat count do
-           (loop for child across (children self) do
-                (visit child))
-           (xmas.render-buffer::translate-scale-rotate 0.0 0.0 1.0 1.0 rot))))
-  (xmas.render-buffer::pop-matrix))
+ (let* ((count (slot-value self 'reflection-count))
+        (rot (/ 360.0 count))
+        (original-rotation (rotation self)))
+      (loop repeat count 
+           for i upfrom 0 do
+           (setf (rotation self) (+ original-rotation (* i rot)))
+           (call-next-method))
+      (setf (rotation self) original-rotation)))
 
 (defclass rect (node) ())
 
 (defmethod draw ((self rect))
   (draw-node-color self)
   (xmas.render-buffer::draw-rect 0 0 (content-width self) (content-height self)))
+
+(defun make-rect (x y colors)
+  (let ((big (make-instance 'rect :content-width 20.0 :content-height 20.0
+                            :x x :y y :color (apply 'vector (first colors))))
+        (small (make-instance 'rect :content-width 5.0 :content-height 5.0
+                              :x 25 :y 25 :color (apply 'vector (first colors)))))
+    (flet ((cycle (node)
+             (run-action node (list
+                               (apply #'tint-to 1.0 (pop colors))
+                               (apply #'tint-to 1.0 (pop colors))
+                               (apply #'tint-to 1.0 (pop colors)))
+                         :repeat :forever)))
+      (cycle big)
+      (cycle small)
+      (add-child big small)
+      big)))
 
 (deftest reflector-node (:width 500 :height 500)
   :init
@@ -44,10 +48,9 @@
              (list 1.0 0.0 0.0)
              (list 0.0 1.0 0.0)
              (list 0.0 0.0 1.0))
-  r1 := (make-instance 'rect :content-width 20.0 :content-height 20.0
-                       :x 50 :y 20 :color (apply 'vector (pop colors)))
-  r2 := (make-instance 'rect :content-width 20.0 :content-height 20.0
-                       :x 50 :y 0 :color (apply 'vector (pop colors)))
+  r1 := (make-rect 50 20 colors)
+  r2 := (make-rect 50 0 colors)
+  r3 := (make-rect 50 40 colors)
   (run-action r1 (list
                   (move-by-x 3.0 -100)
                   (move-by-x 3.0 100))
@@ -56,20 +59,12 @@
                   (move-by-y 3.0 -100)
                   (move-by-y 3.0 100))
               :repeat :forever)
-  (run-action r1 (list
-                  (apply #'tint-to 1.0 (pop colors))
-                  (apply #'tint-to 1.0 (pop colors))
-                  (apply #'tint-to 1.0 (pop colors)))
-              :repeat :forever)
-  (run-action r2 (list
-                  (apply #'tint-to 1.0 (pop colors))
-                  (apply #'tint-to 1.0 (pop colors))
-                  (apply #'tint-to 1.0 (pop colors)))
-              :repeat :forever)
+  
   (run-action r1 (rotate-by 3.0 360) :repeat :forever)
   (run-action r2 (rotate-by 1.5 -360) :repeat :forever)
+  (run-action r3 (rotate-by 3.0 -360) :repeat :forever)
   (run-action root (rotate-by 10.0 -360) :repeat :forever)
-  (add-children root (list r1 r2))
+  (add-children root (list r1 r2 r3))
   :update
   (unless started
     (setf started t)
