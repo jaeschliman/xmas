70ac6bfc2850d183b368ad9edbf3e5ec05f32760
Jason Aeschliman <j.aeschliman@gmail.com>
2018-01-26 16:07:55 -0800

    reflector: more prettier

--- reflector.lisp
+++ reflector.lisp
@@ -26,28 +26,55 @@
 
 (defun make-rect (x y colors)
   (let ((big (make-instance 'rect :content-width 20.0 :content-height 20.0
+                            :opacity 0.5
                             :x x :y y :color (apply 'vector (first colors))))
+        (reflector  (make-instance 'reflector-node :x 25 :y 25 :reflection-count 3))
+        (reflector2 (make-instance 'reflector-node :x 25 :y 25 :reflection-count 5))
+        (med (make-instance 'rect :content-width 10.0 :content-height 10.0
+                            :opacity 0.5
+                            :x 25 :y 25 :color (apply 'vector (first colors))))
         (small (make-instance 'rect :content-width 5.0 :content-height 5.0
-                              :x 25 :y 25 :color (apply 'vector (first colors)))))
+                              :opacity 0.8
+                              :x 55 :y 15 :color (apply 'vector (first colors)))))
     (flet ((cycle (node)
              (run-action node (list
+                               (apply #'tint-to 1.0 (pop colors))
+                               (apply #'tint-to 1.0 (pop colors))
+                               (apply #'tint-to 1.0 (pop colors))
                                (apply #'tint-to 1.0 (pop colors))
                                (apply #'tint-to 1.0 (pop colors))
                                (apply #'tint-to 1.0 (pop colors)))
                          :repeat :forever)))
       (cycle big)
+      (cycle med)
       (cycle small)
-      (add-child big small)
+      (run-action med (rotate-by 6.0 360.0) :repeat :forever)
+      (run-action small (list
+                         (move-by-x 3.0 -50 :ease :in-out-sine)
+                         (move-by-x 3.0 50 :ease :in-out-sine))
+                  :repeat :forever)
+      (run-action med (list
+                       (move-by-y 3.0 -100 :ease :in-out-sine)
+                       (move-by-y 3.0 100 :ease :in-out-sine))
+                  :repeat :forever)
+      (add-child big reflector)
+      (add-child reflector med)
+      (add-child med reflector2)
+      (add-child reflector2 small)
       big)))
 
-(deftest reflector-node (:width 500 :height 500)
+(deftest reflector-node (:width 500 :height 500 :should-clear nil)
   :init
   started := nil
   root := (make-instance 'reflector-node :x 250 :y 250 :reflection-count 5)
+  wash := (make-instance 'rect :content-width 500 :content-height 500 :color (vector 0.3 0.3 0.3) :opacity (/ 1.0 10.0))
   colors := (circular-list
              (list 1.0 0.0 0.0)
+             (list 1.0 1.0 0.0)
              (list 0.0 1.0 0.0)
-             (list 0.0 0.0 1.0))
+             (list 0.0 1.0 1.0)
+             (list 0.0 0.0 1.0)
+             (list 1.0 0.0 1.0))
   r1 := (make-rect 50 20 colors)
   r2 := (make-rect 50 0 colors)
   r3 := (make-rect 50 40 colors)
@@ -63,8 +90,8 @@
   (run-action r1 (rotate-by 3.0 360) :repeat :forever)
   (run-action r2 (rotate-by 1.5 -360) :repeat :forever)
   (run-action r3 (rotate-by 3.0 -360) :repeat :forever)
-  (run-action root (rotate-by 10.0 -360) :repeat :forever)
-  (add-children root (list r1 r2 r3))
+  (run-action root (rotate-by 3.0 -360) :repeat :forever)
+  (add-children root (list wash r1 r2 r3))
   :update
   (unless started
     (setf started t)
