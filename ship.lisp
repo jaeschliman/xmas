d70905fe7aacdf1408d4baa42c07ede31b098d7f
Jason Aeschliman <j.aeschliman@gmail.com>
2018-02-23 07:26:01 -0800

    ship: add kbd controls

--- ship.lisp
+++ ship.lisp
@@ -26,6 +26,16 @@
     (loop repeat (length v) for j upfrom 0 do
          (funcall fn (svref v (mod (+ i j) (length v)))))))
 
+(defmacro do-ring-buffer ((var buffer) &body body)
+  (with-gensyms (lambda)
+    `(let ((,lambda (lambda (,var) ,@body)))
+       (declare (dynamic-extent ,lambda))
+       (ring-buffer-do ,buffer ,lambda))))
+
+(defclass ship (node)
+  ((angle :accessor angle :initform 0.0)
+   (distance :accessor distance :initform 200.0)))
+
 (defconstant pif (coerce pi 'single-float))
 (defconstant 2pif (* 2.0 pif))
 
@@ -70,16 +80,19 @@
   (- (atan (- y 250.0) (- 250.0 x))
      (load-time-value (* pif 0.5))))
 
-(defun project-ship-position (radians &optional (radius 200.0))
+(defun project-ship-position (radians radius)
   (values (+ 250.0 (* (sin radians) radius))
           (+ 250.0 (* (cos radians) radius))))
 
 (defun rad->sprite-deg (rad)
   (* -360.0 (/ rad 2pif)))
 
-(defun update-ship-position (mouse-x mouse-y ship)
+(defun update-ship-position-from-mouse (mouse-x mouse-y ship)
+  (setf (angle ship) (radians-angle-from-center mouse-x mouse-y)))
+
+(defun update-ship-position (ship)
   (multiple-value-bind (x y)
-      (project-ship-position (radians-angle-from-center mouse-x mouse-y))
+      (project-ship-position (angle ship) (distance ship))
     (setf (x ship) x
           (y ship) y
           (rotation ship)
@@ -92,7 +105,7 @@
                        :sprite-frame (get-frame "triangle-32.png")
                        :opacity (+ 0.1 (* 0.8 pct))
                        :scale-x 1.5 :scale-y 2.0)))
-          (run-action sprite (hue-cycle-with-offset 3.0 pct))
+          (run-action sprite (hue-cycle-with-offset 3.0 pct) :repeat :forever)
           sprite))
    'vector))
 
@@ -103,18 +116,17 @@
 
 (defun update-ship-trails (ship trails ring-buffer)
   (let* ((p (ring-buffer-last-element ring-buffer))
-         (i 0)
-         (update (lambda (p) (let ((sprite (svref trails i)))
-                               (setf (x sprite) (p-x p)
-                                     (y sprite) (p-y p)
-                                     (rotation sprite) (p-r p)
-                                     i (1+ i))))))
-    (declare (dynamic-extent update))
+         (i 0))
     (setf (p-x p) (x ship)
           (p-y p) (y ship)
           (p-r p) (rotation ship))
     (ring-buffer-retract ring-buffer)
-    (ring-buffer-do ring-buffer update)))
+    (do-ring-buffer (p ring-buffer)
+      (let ((sprite (svref trails i)))
+        (setf (x sprite) (p-x p)
+              (y sprite) (p-y p)
+              (rotation sprite) (p-r p)
+              i (1+ i))))))
 
 (deftest ship (:width 500 :height 500)
   :init
@@ -124,15 +136,17 @@
                          :texture (texture-frame-texture
                                    (get-frame "soft-ball-32.png")))
   started := nil
+  keys := (make-hash-table :test 'eql)
   mouse-x := 250.0
   mouse-y := 0.0
+  mouse-moved := nil
   globe := (make-instance
             'sprite
             :x 250.0 :y 250.0
             :scale-x 2.0 :scale-y 2.0
             :sprite-frame (get-frame "soft-ball-32.png"))
   ship := (make-instance
-           'sprite
+           'ship
            :opacity 0.8
            :x 250.0 :y 100.0
            :scale-x 1.5 :scale-y 2.0
@@ -152,20 +166,27 @@
     (setf started t)
     (on-enter root))
   (update-stars stars star-sprites dt)
-  (update-ship-position mouse-x mouse-y ship)
+  (cond
+    (mouse-moved (update-ship-position-from-mouse mouse-x mouse-y ship))
+    ((gethash :left keys)  (decf (angle ship) (* 3.0 dt)))
+    ((gethash :right keys) (incf (angle ship) (* 3.0 dt))))
+  (update-ship-position ship) 
   (unless ring-buffer-initialized
     (setf ring-buffer-initialized t)
-    (ring-buffer-do ring-buffer
-                    (lambda (p)
-                      (setf (p-x p) (x ship)
-                            (p-y p) (y ship)
-                            (p-r p) (rotation ship)))))
+    (do-ring-buffer (p ring-buffer)
+      (setf (p-x p) (x ship)
+            (p-y p) (y ship)
+            (p-r p) (rotation ship))))
   (update-ship-trails ship trails ring-buffer)
   (let ((*matrix-stack* stack))
     (visit-with-xform root))
+  (setf mouse-moved nil)
   :on-event
   (case (car event)
     (:mousemove (setf mouse-x (cadr event)
-                      mouse-y (cddr event)))))
+                      mouse-y (cddr event)
+                      mouse-moved t))
+    (:keydown (setf (gethash (cdr event) keys) t))
+    (:keyup   (setf (gethash (cdr event) keys) nil))))
 
 (run-test 'ship)
